52aa0812b171d00dd340a34048ea1a1472dc6f25
dakaii <daiki815@gmail.com>
2017-05-28 22:34:57 +0900

    Initial Commit

---
+++ Where Was I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Where Was I
+//
+//  Created by Daiki Nakashita on 5/28/17.
+//  Copyright © 2017 Daiki Nakashita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
